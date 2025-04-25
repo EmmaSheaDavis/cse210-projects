f513307d1484d53548a6e647d06f302e7a8a1d5e
EmmaSheaDavis <emmadavis@byui.edu>
2025-04-25 09:07:01 -0600

    Prep 2 Letter Grade with plus and minus

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string letter = "";
+
+        string sign = "";
+        int lastDigit = (int)percentage % 10;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else{
+            sign = "";
+        }
+
+        if (letter == "A" || letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Youre Grade: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You have passed the class!");
+        }
+        else{
+            Console.WriteLine("You have failed the class. You'll get it next time!");
+        }
     }
 }
